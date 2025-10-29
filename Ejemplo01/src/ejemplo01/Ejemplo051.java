6f8ffedca12d4b107967511e65018d17ec449c51
Adrian Lopez <israelruiz2874@gmail.com>
2025-10-29 12:17:39 -0500

    Subir al repositorio

--- Ejemplo01/src/ejemplo01/Ejemplo051.java
+++ Ejemplo01/src/ejemplo01/Ejemplo051.java
@@ -11,23 +11,23 @@ package ejemplo01;
  *
  * @author reroes
  */
-public class Ejemplo05 {
+public class Ejemplo051 {
     public static void main(String[] args) {
         // Secuencia de escape \n y \t
         String nombreEstudiante = "René Rolando";
         String apellidoEstudiante = "Elizalde Solano";
         int nacimiento = 1983;
         
-        /*System.out.println(nombreEstudiante+"\n"+apellidoEstudiante+ "\n"+ 
+        System.out.println(nombreEstudiante+"\n"+apellidoEstudiante+ "\n"+ 
                 nacimiento);
         
         System.out.println(nombreEstudiante+"\n\n"+apellidoEstudiante+ "\n\n"+ 
                 nacimiento);
         
         System.out.println(nombreEstudiante+"\n"+"\n"+apellidoEstudiante+ "\n"+
-                "\n"+ nacimiento); */
-        System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
-        System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante); 
+                "\n"+ nacimiento); 
+        //System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
+        //System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante); 
         
         // el "/n" este sirve para crear un salto de linea 
         // el "/t" este sirve para crear una tabulacion horizontal
